497ff47fd5dcc5e8f42ea9c0fc8af35d4364fb4e
Phil <robertspb@yahoo.com>
2020-09-22 09:13:07 -0500

    first draft multi-line plot by state

--- tt.r
+++ tt.r
@@ -1,2 +1,21 @@
 library(tidytuesdayR)
+library(ggplot2)
+library(dplyr)
 tt_data <- tt_load("2020-09-15") #govt spend on kids
+
+View(tt_data)
+readme(tt_data)
+print(tt_data)
+View(tt_data$kids)
+
+df <- data.frame(tt_data$kids) %>%
+  filter(variable %in% c('PK12ed'))
+
+ggplot(df, aes(x=year, y=inf_adj_perchild, color=state)) +
+  geom_line() 
+
+# todo: 
+# - color by region
+# - changepoint
+# - cluster by outcome
+# - loess
